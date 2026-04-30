4f5429d164cdabe0304220f734775fd4f1f9bf36
Tamas Szalay <tamas.szalay@seon.io>
2026-04-30 14:53:13 +0200

    Update readme and package descriptor

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SeonStreamSDK",
+    platforms: [.iOS(.v13.0)],
+    products: [
+        .library(
+            name: "SeonStreamSDK",
+            targets: ["SeonStreamSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SeonStreamSDK",
+            url: "https://cdn.seon.io/sdk/ios/SeonStreamSDK/1.0.0/SeonStreamSDK-1.0.0-xcfw.zip",
+            checksum: "3f0183451e81bf7498cc179655133995c959ddebde7c9eedd369a581ef80bdcb"
+        )
+    ]
+)
